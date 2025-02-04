116832329706359bb87b176a1ae1865cdc9f4e1d
Ashish M <ashish@ashish.org>
2025-02-04 14:26:57 -0800

    darkened app container background - edited index.css --card attribute

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ import Practice from './components/Practice.jsx';
 
 function App() {
   return (
-          <div className="min-h-screen bg-gray-300 p-4">
-              <div className="container mx-auto max-w-4xl bg-white rounded-3xl border shadow-sm overflow-hidden">
-                  <Practice/>
-              </div>
+      <div className="min-h-screen bg-gradient-to-b from-[#E6E6FA] to-[#E1F5FE] p-4">
+          <div className="container mx-auto max-w-4xl bg-white rounded-3xl border shadow-sm overflow-hidden">
+              <Practice/>
           </div>
+      </div>
   );
 }
 
